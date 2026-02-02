7bac5b7649c39c8cd66993b12800a19a463b545c
Adyasha mishra <adyashamishra@Mac.lan>
2026-02-02 12:22:07 +0530

    Week1: week1 practice1 (input, arithmatic, even_odd)

---
+++ Week-1/week1 practice1/02_arithmatic.py
@@ -0,0 +1,19 @@
+#_____________________________________________________________________________________________________________
+#File: 02_arithmatic.py
+#Purpose: Practice
+#Concept: 
+# - input
+# _ variable
+# - arithmatic
+#______________________________________________________________________________________________________________
+
+a = int(input("enter first number:"))#20
+b = int(input("enter second number:"))#2
+
+print("sum:", a + b) #22
+print("diff:", a - b) #18
+print("product:", a * b) #40
+print("division:", a / b) #exat division (always gives floating value) 10.0
+print("division:", a // b) # gives intiger (10) 
+print("remainder:", a % b) #0
+print("power:", a ** b) #400
---
+++ Week-1/week1 practice1/03_type_check.py
@@ -0,0 +1,19 @@
+#_____________________________________________________________________________________________________________
+#File: 03_type_check.py
+#Purpose: Practice
+#Concept:
+# -Data type
+# -Print
+#____________________________________________________________________________________________________________
+
+x = 20
+y = "56"
+z = 10.9
+a = True
+b = None
+
+print(type(x)) #int
+print(type(y)) #"str"
+print(type(z)) #float
+print(type(a)) #bool
+print(type(b)) #NoneType
